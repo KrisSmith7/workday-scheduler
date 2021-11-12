fdb923c92cef851d18d642440a5ed23d692d2bf4
KrisSmith7 <krissmithcodes@gmail.com>
2021-11-11 20:03:18 -0500

    added time blocks to html

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,40 @@ WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
 */
 var dayContainer = document.getElementById("currentDay");
-var currentDay = moment().date(Number);
+var currentDay = moment().format("dddd, MMMM Do YYYY");
 
 var displayDate = function () {
     dayContainer.textContent = currentDay;
+};
 
-}
+/*
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours*/
+
+var taskContainer = document.querySelector(".container")
 
 
 /*
-
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
-
-
 WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
+THEN each time block is color-coded to indicate whether it is in the past, present, or future 
+var currentHour = (new Date()).getHours();
+$('.colorcode')
+  .each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour + 6){
+      $(this).css('background-color','Blue');
+    }else if(val < currentHour && val > currentHour-6){
+      $(this).css('background-color','Red');
+    }else if(val === currentHour){
+      $(this).css('background-color','Green');
+    }else{
+      $(this).css('background-color','White');
+    }
+  });
+  */
 
 
-WHEN I click into a time block
+/*WHEN I click into a time block
 THEN I can enter an event
 
 WHEN I click the save button for that time block
