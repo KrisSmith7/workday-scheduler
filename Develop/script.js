1caa99051832db30e2ce0c2386508657ab15fca1
KrisSmith7 <krissmithcodes@gmail.com>
2021-11-14 21:44:39 -0500

    updated comments

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ var displayDate = function () {
 $(".time-block").each(function() {
     var timeBlocks = $(this).attr("id").split("m")[1]; //uses 24 hr time and splits id attribute in HTML to crosscheck time to add appropriate css class
     // console.log(timeBlocks);
-    console.log (currentHour)
+    // console.log (currentHour);
     if (currentHour === timeBlocks) {
         $(this).addClass("present");
     } else if (currentHour < timeBlocks ) {
