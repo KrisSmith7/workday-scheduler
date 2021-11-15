2dff4b1a5810884438f7848e745d6ce2eebf8671
KrisSmith7 <krissmithcodes@gmail.com>
2021-11-14 21:42:06 -0500

    color coded timeBlocks/respond to current hour of day

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,21 @@ var displayDate = function () {
     dayContainer.textContent = currentDay;
 };
 
+//checks current time and updates colorcode for each time-block
+$(".time-block").each(function() {
+    var timeBlocks = $(this).attr("id").split("m")[1]; //uses 24 hr time and splits id attribute in HTML to crosscheck time to add appropriate css class
+    // console.log(timeBlocks);
+    console.log (currentHour)
+    if (currentHour === timeBlocks) {
+        $(this).addClass("present");
+    } else if (currentHour < timeBlocks ) {
+    $(this).addClass("future");
+} else if (currentHour > timeBlocks) {
+    $(this).addClass("past");
+}
+});
+
+
 
 
 
